4d53a08a7db88add7b92f85dc9f94a9485144bbf
Kingsley Victor <javaprodigy56@gmail.com>
2022-07-28 14:14:53 +0100

    init repo

---
+++ contracts/TransactionProxy.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+contract TransactionProxy is AccessControl {}
