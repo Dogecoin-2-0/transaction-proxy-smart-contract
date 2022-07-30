fd2f3d266780384c6d672ef9c83426c5f028556e
Kingsley Victor <javaprodigy56@gmail.com>
2022-07-30 10:44:54 +0100

    ...

--- contracts/helpers/TransferHelpers.sol
+++ contracts/helpers/TransferHelpers.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/utils/Address.sol';
+import "@openzeppelin/contracts/utils/Address.sol";
 
 library TransferHelpers {
   using Address for address;
 
   function _safeTransferEther(address to, uint256 amount) internal returns (bool success) {
     (success, ) = to.call{value: amount}(new bytes(0));
-    require(success, 'failed to transfer ether');
+    require(success, "failed to transfer ether");
   }
 
   function _safeTransferERC20(
@@ -15,9 +15,9 @@ library TransferHelpers {
     address to,
     uint256 amount
   ) internal returns (bool success) {
-    require(token.isContract(), 'call_to_non_contract');
-    (success, ) = token.call(abi.encodeWithSelector(bytes4(keccak256(bytes('transfer(address,uint256)'))), to, amount));
-    require(success, 'low_level_contract_call_failed');
+    require(token.isContract(), "call_to_non_contract");
+    (success, ) = token.call(abi.encodeWithSelector(bytes4(keccak256(bytes("transfer(address,uint256)"))), to, amount));
+    require(success, "low_level_contract_call_failed");
   }
 
   function _safeTransferFromERC20(
@@ -26,15 +26,15 @@ library TransferHelpers {
     address recipient,
     uint256 amount
   ) internal returns (bool success) {
-    require(token.isContract(), 'call_to_non_contract');
+    require(token.isContract(), "call_to_non_contract");
     (success, ) = token.call(
       abi.encodeWithSelector(
-        bytes4(keccak256(bytes('transferFrom(address,address,uint256)'))),
+        bytes4(keccak256(bytes("transferFrom(address,address,uint256)"))),
         spender,
         recipient,
         amount
       )
     );
-    require(success, 'low_level_contract_call_failed');
+    require(success, "low_level_contract_call_failed");
   }
 }
