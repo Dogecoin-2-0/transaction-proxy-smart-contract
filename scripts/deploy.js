01b768402b3a3a82a137595988df76bae4831b3a
Kingsley Victor <javaprodigy56@gmail.com>
2022-08-08 17:58:26 +0100

    dep...

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const { ethers, network } = require("hardhat");
 
 async function deploy() {
@@ -5,6 +7,19 @@ async function deploy() {
   const TransactionProxyFactory = await ethers.getContractFactory("TransactionProxy");
   let proxy = await TransactionProxyFactory.deploy();
   proxy = await proxy.deployed();
+
+  const location = path.join(__dirname, "../addresses.json");
+  const fileExists = fs.existsSync(location);
+
+  if (fileExists) {
+    const buf = fs.readFileSync(location);
+    const val = JSON.parse(buf.toString());
+    const updatedVal = { ...val, [network.name]: proxy.address };
+
+    fs.writeFileSync(location, JSON.stringify(updatedVal, undefined, 2));
+  } else {
+    fs.writeFileSync(location, JSON.stringify({ [network.name]: proxy.address }, undefined, 2));
+  }
 
   console.log("Contract deployed on address ", proxy.address);
 }
